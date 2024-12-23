d004bc9c7fdb2348d4d0e870a17b8915cda76c13
sushichan044 <mail@sushichan.live>
2024-12-23 13:21:01 +0900

    add description

---
+++ playground/google.js
@@ -0,0 +1,20 @@
+// @ts-check
+
+import { createUnifont, providers } from 'unifont'
+
+import { createStorage } from 'unstorage'
+import fsDriver from 'unstorage/drivers/fs-lite'
+
+const storage = createStorage({
+  driver: fsDriver({ base: 'output' }),
+})
+
+const unifont = await createUnifont([
+  providers.google(),
+], { storage })
+
+const fonts = await unifont.resolveFont('Noto Sans JP')
+const minimizedFonts = await unifont.resolveFont('Noto Sans JP', { glyphs: ['Hello, World!'] })
+
+console.log(fonts)
+console.log(minimizedFonts)
